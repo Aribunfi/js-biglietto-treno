dd571616814c04484fb4901d9871575103054425
Aribunfi <arianna@pabis.de>
2023-01-24 13:25:57 +0100

    provo a sbrogliare

--- action.js
+++ action.js
@@ -6,28 +6,28 @@ console.log('Quanti km devi percorrere?' + numeroKm);
 // chiedo all'utente la sua età
 
 const userAge = prompt('Quanti anni hai?')
-console.log('Quanti anni hai?' + quantiAnni);
+console.log('Quanti anni hai?' + userAge);
 
 
 
 // prima regola: ogni km va moltiplicato per 0.21 euro
 
-let costoBiglietto = numeroKm * 0.21
-
-document.getElementById('quantoPaghi').innerHTML;
-
-const quantoPaghi = per fare $('numeroKm') pagherai $('quantoPaghi');
+let costoBiglietto = numeroKm * 0.21;
+let outputMessage;
+outputMessage = ('Il prezzo è: ${costoBiglietto}');
+console.log(outputMessage);
 
 // seconda regola: se l'età dell'utente è <18 anni al prezzo risultante dalla moltiplicazione si applica uno sconto del 20%
 
 
-if (userAge<18) {
-
+if (userAge < 18) {
+    const discountUnder18 = costoBiglietto * 20 / 100;
 }
 
 // terza regola: se l'età dell'utente è > 65 anni al prezzo risultante dalla moltiplicazione si applica uno sconto del 40%
 
-if (userAge >65) {
+if (userAge > 65) {
+
 
 }
 
