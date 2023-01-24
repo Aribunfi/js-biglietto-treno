7eceacb39be2c6f885be350fe2f8f6c80db0cb70
Aribunfi <arianna@pabis.de>
2023-01-24 15:41:49 +0100

    fine esercizio

--- action.js
+++ action.js
@@ -14,7 +14,7 @@ console.log('Quanti anni hai?' + userAge);
 
 let costoBiglietto = numeroKm * 0.21;
 let outputMessage;
-outputMessage = ('Il prezzo è: ${costoBiglietto}');
+outputMessage = (`Il prezzo è: ${costoBiglietto}`);
 console.log(outputMessage);
 
 // seconda regola: se l'età dell'utente è <18 anni al prezzo risultante dalla moltiplicazione si applica uno sconto del 20%
@@ -22,13 +22,21 @@ console.log(outputMessage);
 
 if (userAge < 18) {
     const discountUnder18 = costoBiglietto * 20 / 100;
+    costoBiglietto = costoBiglietto-discountUnder18;
 }
 
 // terza regola: se l'età dell'utente è > 65 anni al prezzo risultante dalla moltiplicazione si applica uno sconto del 40%
 
-if (userAge > 65) {
+else if (userAge > 65) {
 
-
+    const discountOver65 = costoBiglietto * 40 / 100;
+    costoBiglietto = costoBiglietto-discountOver65; 
 }
 
+else {
+
+}
 // output del prezzo totale deve apparire con due decimali
+console.log(costoBiglietto);
+
+document.getElementById('outputMessage').innerHTML = "costo biglietto: " + costoBiglietto;
